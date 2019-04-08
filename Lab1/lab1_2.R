0dfa571ffd0c2619c67467e77e1c7e989fe4e8aa
Sophie Lindberg <sopli268@student.liu.se>
2019-04-08 11:55:58 +0200

    lab1_2 finished

--- Lab1/lab1_2.R
+++ Lab1/lab1_2.R
@@ -3,6 +3,7 @@ library(invgamma)
 income <- c(14,25,45,25,30,33,19,50,34,67)
 m <- 3.5
 n <- length(income)
+n_draws = 10000
 
 t <- 0
 
@@ -11,6 +12,13 @@ for (v in income) {
 }
 
 tSquared <- t / n
+
+# simulate postarior draws
+
+X_draw <- rchisq(n_draws, n)
+sigma_sq = n*tSquared / X_draw
+
+#theoretical
 
 theoretical <- function(theta, v, s) {
   return ( ((v/2) ^ (v/2)) / gamma(v/2)  * (s ^ v) * (theta ^ (-(v/2+1))) * exp((-v * s ^ 2) / (2 * theta) ))
@@ -23,19 +31,7 @@ for (i in 1:length(range)) {
   mx[i] <- theoretical(range[i], length(income), sqrt(tSquared))
 }
 
-# Given chisquared
-NormalNonInfoPrior<-function(NDraws,Data){
-  Datamean<-mean(Data)
-  s2<-var(Data)
-  n<-length(Data)
-  PostDraws=matrix(0,NDraws,2)
-  PostDraws[,2]<-((n-1)*s2)/rchisq(NDraws,n-1)
-  PostDraws[,1]<-Datamean+rnorm(NDraws,0,1)*sqrt(PostDraws[,2]/n)
-  
-  return(PostDraws)
-}
+# plot
+hist(sigma_sq, freq=FALSE) 
+lines(range, mx, lwd=3, type='l', col='red', xlab = 'sigma')
 
-Data <- c(14,25,45,25,30,33,19,50,34,67) 			# Sampling 100 observations from the N(5,10) density##
-PostDraws <- NormalNonInfoPrior(10000, Data) # Generating 1000 draws from the joint posterior density of mu and sigma^2
-hist(PostDraws[,1], freq=FALSE) 			# Plotting the histogram of mu-draws
-lines(mx, lwd=3, type='l', col='red')
