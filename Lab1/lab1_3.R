e4713cfe857e7bf6264257356ceef15e81ee3009
Arvid Edenheim <arved490@student.liu.se>
2019-04-11 21:22:29 +0200

    Exercise 3 done

--- Lab1/lab1_3.R
+++ Lab1/lab1_3.R
@@ -1,18 +1,30 @@
 
-# A
+# A: Plot the posterior distribution of k
+# posterior(k | y, mu) = likelihood * prior(k) = prod_prob * prior
+
 y <- c(-2.44, 2.14, 2.54, 1.83, 2.01, 2.33, -2.79, 2.23, 2.07, 2.02)
-k <- seq(0,10, by=0.01)
+kSeq <- seq(0,10, by=0.01)
 mu <- 2.39
 lambda <- 1
-kappa <- dexp(x,lambda)
+# kappa <- dexp(kSeq, lambda)
 
-plot(k, kappa, type = 'l')
-
-# B -> compute the posterior mode
-
-Mises <- function(kappa, y, mu) {
-  I <- besselI(kappa,0)
-  return (exp(kappa* cos (y-mu)))/(2*pi*I)
+mises <- function(k, y, mu) {
+  I <- besselI(k,0)
+  return ((exp(k * cos(y - mu))) / (2 * pi * I))
 }
 
-likelihood = Mises(kappa[1], y, mu)
+kPos <- function(k, mu,y) {
+  return ( prod( mises(k, y, mu) ) * dexp(k))
+}
+
+posterior = c()
+for (k in kSeq){
+  posterior = c(posterior, c(kPos(k, mu, y)))
+}
+
+plot(kSeq, posterior)
+
+
+# B: Compute the posterior mode of k
+
+kPosMode <- kSeq[which.max(posterior)]
