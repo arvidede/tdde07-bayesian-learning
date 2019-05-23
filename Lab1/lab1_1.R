28893fd387c4347411f7e4ad392ec2fbd1f60b95
Sophie Lindberg <sopli268@student.liu.se>
2019-05-23 11:24:25 +0200

    fixed lab 1

--- Lab1/lab1_1.R
+++ Lab1/lab1_1.R
@@ -3,6 +3,7 @@ beta <- 2
 s <- 14
 f <- 6
 n <- 20
+f <- n-s
 nDraws <- c(10, 100, 1000)
 
 # 1a
@@ -17,30 +18,50 @@ nDraws <- c(10, 100, 1000)
 
 xGrid <- seq(0.001, 0.999, by=0.001)
 posterior = dbeta(xGrid, alpha + s, beta + (n-s))
+trueMean <- (alpha + s)/(alpha + beta + s + f)
+trueStdv <- sqrt(((alpha + s) * ( beta + f)) / ((alpha + beta + n)^2 * (alpha + beta + n + 1)))
 par(mfrow=c(1,3))
 for (draws in nDraws) {
   random = rbeta(draws, alpha+s, beta+(n-s))
   mean <- mean(random)
   stdv <- sd(random)
-  trueMean <- (alpha + s)/(alpha + s + beta + f)
-  trueStdv <- (sqrt((alpha + s)*(beta + f)/((alpha + s + beta + f)^2 * (alpha + s + beta + f + 1))))
   hist(random, xlim = c(0,1), freq = FALSE, breaks=10)
   lines(xGrid, posterior, type='l', col ='red')
-  
 }
 dev.off() 
 
-# 1b
+mean <- c()
+stdv <- c()
+for (i in 1:1000) {
+  random = rbeta(i, alpha+s, beta+(n-s))
+  mean <- c(mean, mean(random))
+  stdv <- c(stdv, sd(random))
+}
 
+# plot mean
+plot(mean, type='l', col='green', xlab = 'n draws')
+abline(h = trueMean)
+legend('topright', legend = 'True mean', col = 'black', lwd = 1)
+
+# Plot standard diviation 
+plot(stdv, type='l', col='black', xlab='n draws')
+abline(h = trueStdv, col='red')
+legend('topright', legend = 'True stdv', col = 'red', lwd = 1)
+
+
+
+# 1b
 nDraws <- 10000
 random = rbeta(nDraws, alpha+s, beta+(n-s))
-val <- random * (random < 0.4)
-print(sum(val>0)/length(val))
+print(sum(random < 0.4)/length(random))
 real <- pbeta(.4, alpha + s, beta + (n-s))
 
 # 1c
+##you compute random draws of phi, but then you plot the density of the 
+##original parameter theta. Plot the density of phi instead. 
+##Also the mathematical statement should say theta instead of phi on the right hand side.
+
 nDraws <- 10000
 random = rbeta(nDraws, alpha+s, beta+(n-s))
 phi <- log(random/(1 - random))
-plot(density(random), lwd=3, type='l', col='red')
-
+plot(density(phi), lwd=1, type='l', col='red', main = expression('Density of' ~ phi))
--- Lab1/lab1_3.R
+++ Lab1/lab1_3.R
@@ -2,6 +2,11 @@
 # A: Plot the posterior distribution of k
 # posterior(k | y, mu) = likelihood * prior(k) = prod_prob * prior
 
+#You have plotted the unnormalized posterior correctly, 
+#but you should plot the normalized posterior that has area under the curve = 1. 
+#To do this, you need to compute the sum of your function values and also 
+#account for the spacing between your gridpoints and divide the function with the right value.
+
 y <- c(-2.44, 2.14, 2.54, 1.83, 2.01, 2.33, -2.79, 2.23, 2.07, 2.02)
 kSeq <- seq(0,10, by=0.01)
 mu <- 2.39
@@ -23,9 +28,10 @@ for (k in kSeq){
   posterior = c(posterior, c(kPos(k, mu, y)))
 }
 
-plot(kSeq, posterior,type='l')
+##Plot the normalized posterior
+normConst <- sum(posterior)
+plot(kSeq, posterior/normConst,type='l')
 legend('topright', legend='Mode', fill='red')
-
 
 # B: Compute the posterior mode of k
 
