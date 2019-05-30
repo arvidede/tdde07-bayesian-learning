910bdfe686840f8448dd36c4c6506e6edbff651b
Arvid Edenheim <arved490@student.liu.se>
2019-05-30 10:54:38 +0200

    Refactor lab1

--- Lab1/lab1_1.R
+++ Lab1/lab1_1.R
@@ -6,15 +6,7 @@ n <- 20
 f <- n-s
 nDraws <- c(10, 100, 1000)
 
-# 1a
-# What are the true values of the posterior mean and standard deviation? 
-# Look at the Wikipedia page for the beta distribution or in the table of 
-# distributions on the course webpage. Compute these based on the posterior parameters. 
-# Then compute the same values based on the posterior samples 
-# (using mean() and sd() in R) and see if you get the same results.
-# To show the convergence, make a plot that shows the posterior mean and standard deviation on the y-axis, 
-# computed based on posterior samples and number of samples on the x-axis. 
-# You can then also include the true values in this plot.
+# 1a - Random draws from posterior of θ | y ~ Beta(α + s, β + f)
 
 xGrid <- seq(0.001, 0.999, by=0.001)
 posterior = dbeta(xGrid, alpha + s, beta + (n-s))
@@ -28,6 +20,8 @@ for (draws in nDraws) {
 }
 dev.off() 
 
+par(mfrow=c(1,2))
+# Mean and standard deviation
 mean <- c()
 stdv <- c()
 for (i in 1:1000) {
@@ -36,29 +30,25 @@ for (i in 1:1000) {
   stdv <- c(stdv, sd(random))
 }
 
-# plot mean
+# Plot mean to see convergence
 plot(mean, type='l', col='green', xlab = 'n draws')
 abline(h = trueMean)
 legend('topright', legend = 'True mean', col = 'black', lwd = 1)
 
-# Plot standard diviation 
+# Plot standard diviation to see convergence
 plot(stdv, type='l', col='black', xlab='n draws')
 abline(h = trueStdv, col='red')
 legend('topright', legend = 'True stdv', col = 'red', lwd = 1)
 
 
-
-# 1b
+# 1b - Simulation of Pr(θ < 0.4|y), compared with the real value using pbeta
 nDraws <- 10000
 random = rbeta(nDraws, alpha+s, beta+(n-s))
 print(sum(random < 0.4)/length(random))
 real <- pbeta(.4, alpha + s, beta + (n-s))
 
-# 1c
-##you compute random draws of phi, but then you plot the density of the 
-##original parameter theta. Plot the density of phi instead. 
-##Also the mathematical statement should say theta instead of phi on the right hand side.
 
+# 1c - Posterior distribution of the log-odds by simulation
 nDraws <- 10000
 random = rbeta(nDraws, alpha+s, beta+(n-s))
 phi <- log(random/(1 - random))
--- Lab1/lab1_3.R
+++ Lab1/lab1_3.R
@@ -1,11 +1,7 @@
 
 # A: Plot the posterior distribution of k
 # posterior(k | y, mu) = likelihood * prior(k) = prod_prob * prior
-
-#You have plotted the unnormalized posterior correctly, 
-#but you should plot the normalized posterior that has area under the curve = 1. 
-#To do this, you need to compute the sum of your function values and also 
-#account for the spacing between your gridpoints and divide the function with the right value.
+# Plot of the normalized posterior with AUC = 1
 
 y <- c(-2.44, 2.14, 2.54, 1.83, 2.01, 2.33, -2.79, 2.23, 2.07, 2.02)
 gridWidth <- 0.01
@@ -29,9 +25,9 @@ for (k in kSeq){
   posterior = c(posterior, c(kPos(k, mu, y)))
 }
 
-##Plot the normalized posterior
+# Plot the normalized posterior with AUC = 1
 normConst <- sum(posterior)
-plot(kSeq, posterior/(normConst*gridWidth),type='l')
+plot(kSeq, posterior/(normConst*gridWidth),type='l', ylab = 'Normalized Density')
 legend('topright', legend='Mode', fill='red')
 
 # B: Compute the posterior mode of k
